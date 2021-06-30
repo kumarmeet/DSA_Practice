1cfd9227faaddce34fef464b938963cc642da507
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-07-01 00:00:35 +0530

    Circular Singly Linked List
    In this circular singly linked list has different approach i have used. In this approach i used last pointer instead of start/head pointer. 
    
    Benefits if you make last pointer to operate the CSLL is when you insert last node simply add that node in last position without using loop or recursion or add a node beginning of the linked list it is same we don't want to traverse the entire list just add node at the beginning of CSLL because last pointer contain the last node address and last->next contain the beginning node address. 
    
    add last node and beginning both time complexity is O(1) means constant time

---
+++ CSLL.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node* create_node(int);
+
+struct node
+{
+    int info;
+    struct node *next;
+};
+
+struct node* search_node(struct node *last, int item)
+{
+    struct node *r = last;
+    if(!last)
+        return NULL;
+    r = last->next;
+    do
+    {
+        if(r->info == item)
+            return r;
+        r = r->next;
+    }
+    while(r->next != last->next);
+
+    return NULL;
+}
+
+void display(struct node *last)
+{
+    struct node *t;
+
+    if(last == NULL)
+        return;
+    else
+    {
+        t = last->next;
+        do
+        {
+            printf("%d ",t->info);
+            t=t->next;
+        }
+        while(t != last->next);
+    }
+}
+
+void delete_node(struct node **last, int item)
+{
+    struct node *r, *t;
+    r = (*last)->next;
+
+    if(!(*last))
+        return;
+    if(r->info == item)
+        delete_node_begining(last);
+    else
+    {
+        while(search_node(r, item))
+        {
+            t = r;
+            r = r->next;
+        }
+
+        t->next = r->next;
+        *last = t;
+        free(r);
+    }
+}
+
+
+void delete_node_begining(struct node **last)
+{
+    if(*last == NULL)
+        return;
+    if((*last)->next == *last)
+    {
+        free(*last);
+        *last = NULL;
+    }
+    else
+    {
+        struct node *r = (*last)->next;
+        (*last)->next = r->next;
+        free(r);
+    }
+
+}
+
+void insert_node_begining(struct node **last, int item)
+{
+    struct node *n = create_node(item);
+
+    if(*last == NULL)
+        *last = n;
+    else
+    {
+        n->next = (*last)->next;
+        (*last)->next = n;
+    }
+}
+
+void delete_node_last(struct node **last)
+{
+    struct node *r;
+    if(*last == NULL)
+        return;
+    if((*last)->next == *last)
+    {
+        free(*last);
+        *last = NULL;
+    }
+    else
+    {
+        r = *last;
+        while(r->next != *last)
+            r = r->next;
+        r->next = (*last)->next;
+        free(*last);
+        *last = r;
+    }
+}
+
+void insert_node_last(struct node **last, int item)
+{
+    struct node *n = create_node(item);
+
+    if(!(*last))
+        *last = n;
+    else
+    {
+        n->next = (*last)->next;
+        (*last)->next = n;
+    }
+
+    *last = n;
+}
+
+struct node* create_node(int item)
+{
+    struct node *n;
+    n = (struct node*)malloc(sizeof(struct node));
+    n->info = item;
+    n->next = n;
+    return n;
+}
